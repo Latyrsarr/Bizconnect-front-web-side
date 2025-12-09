83835b7cb377150df4991420893df982850110f2
Latyrsarr <134410504+Latyrsarr@users.noreply.github.com>
2025-12-09 09:26:38 +0000

    update 09

--- pages/profil/[code].vue
+++ pages/profil/[code].vue
@@ -466,7 +466,7 @@ async function fetchProfile(codeParam: string) {
 
   try {
     // NOTE: en prod, utilisez une variable d'environnement pour baseURL
-    const { data } = await axios.get<Profile>(`https://bizconnect-back-web-side.onrender.com/profiles/code/${encodeURIComponent(codeParam)}`)
+    const { data } = await axios.get<Profile>(`https://bizconnect-back-web-side.vercel.app/profiles/code/${encodeURIComponent(codeParam)}`)
     profile.value = data
   } catch (err) {
     console.error('Erreur lors de la récupération du profil :', err)
