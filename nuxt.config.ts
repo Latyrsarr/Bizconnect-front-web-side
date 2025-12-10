b3e9aebfabcaecd944534e7aaf5c9c8ab1de7d5d
Latyrsarr <134410504+Latyrsarr@users.noreply.github.com>
2025-12-10 09:25:26 +0000

    update 10

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -4,5 +4,13 @@ export default defineNuxtConfig({
   ],
   css: [
     '@/assets/css/main.css'
-  ]
+  ],
+  app: {
+    head: {
+      link: [
+        { rel: "icon", type: "image/x-icon", href: "/favicon.ico" },
+        { rel: "icon", type: "image/svg", href: "/bizconnect.svg" }
+      ]
+    }
+  }
 })
